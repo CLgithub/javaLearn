be9a16f453100fbb4ccda4b358eb2dbb17f4e1da
CLgithub <1064134436@qq.com>
2016-02-25 10:17:18 +0800

    加入bFull控制两个线程协调工作

--- src/com/cl/learn/thread/Q.java
+++ src/com/cl/learn/thread/Q.java
@@ -4,6 +4,7 @@ public class Q {
 
 	private String name;
 	private String gender;
+	private Boolean bFull=false;
 
 	public String getName() {
 		return name;
@@ -27,18 +28,27 @@ public class Q {
 	 * @date 2016年2月25日
 	 */
 	public synchronized void producer(int i){
-			if(i==0){
-				this.setName("小明");
-				try {
-					Thread.sleep(10);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				this.setGender("男");
-			}else {
-				this.setName("小红");
-				this.setGender("女");
+		if(bFull){//如果为true，表示已经存入，则该线程进入睡眠等待
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
+		}
+		if(i==0){
+			this.setName("小明");
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			this.setGender("男");
+		}else {
+			this.setName("小红");
+			this.setGender("女");
+		}
+		bFull=true;//存入后就设为ture
+		notify();//唤醒改线程
 	}
 	
 	/**
@@ -47,6 +57,15 @@ public class Q {
 	 * @date 2016年2月25日
 	 */
 	public synchronized void consumer(Q q){
+		if(!bFull){//如果为false，表示已经取走，则该线程睡眠等待
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		System.out.println(q.getName()+"--->"+q.getGender());
+		bFull=false;//取走后就设为false
+		notify();//唤醒改线程
 	}
 }
