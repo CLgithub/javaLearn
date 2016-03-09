45d6a5e02dcd53e13fc980907df1eb2014f1094d
CLgithub <1064134436@qq.com>
2016-03-09 14:26:43 +0800

    父类与子类

---
+++ src/com/cl/javabasis/day7/MyDemo2.java
@@ -0,0 +1,14 @@
+package com.cl.javabasis.day7;
+
+public class MyDemo2 {
+	public static void main(String[] args) {
+		Student stu = new Student(1);
+//		stu.name="a";//子类对象访问父类非静态成员
+//		stu.age=12;//子类对象访问父类静态成员
+//		stu.method1();//子类对象访问父类非静态方法
+//		stu.method2();//子类对象访问父类静态方法
+		
+//		stu.method2();
+//		System.out.println(stu);
+	}
+}
---
+++ src/com/cl/javabasis/day7/Person.java
@@ -0,0 +1,57 @@
+package com.cl.javabasis.day7;
+
+class Person {
+	static{
+		System.out.println("父类静态初始化代码块！");
+	}
+	{System.out.println("父类初始化代码块！");}
+	public Person() {
+		System.out.println("父类无参构造函数！");
+	}
+	public Person(int i) {
+		System.out.println("父类有参构造函数！");
+	}
+	
+	String name;
+	static int age;
+	
+	void method1(){
+		System.out.println("父类非静态方法");
+	}
+	
+	static void method2(){
+		System.out.println("父类静态方法");
+	}
+
+}
+
+class Student extends Person {
+	static{
+		System.out.println("子类静态初始化代码块！");
+	}
+	{System.out.println("子类初始化代码块！");}
+	public Student() {
+		System.out.println("子类无参构造函数！");
+	}
+	public Student(int i) {
+		super(1);//调用父类有参构造函数
+		System.out.println("子类有参构造函数！");
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + super.name + ",age=" + this.age + "]";
+	}
+
+	@Override
+	void method1() {
+		super.age=1;
+		System.out.println("子类非静态方法");
+	}
+	
+	
+	
+	
+	
+}
+
