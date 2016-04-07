3ae856fc4785c887257cb5c95950817ad4bedd54
Lhome <1064134436@qq.com>
2016-04-07 12:38:21 +0800

    Arrays和Collections工具类

---
+++ src/com/cl/javabasis/day17map/MyDemo7.java
@@ -0,0 +1,108 @@
+package com.cl.javabasis.day17map;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/*
+集合的工具类（Collections）
+
+笔试题：说出Collection与Collections的区别
+	1.Collection是单列集合的根接口，Collections是操作集合的工具类
+
+Collections：常见方法：
+	1，对list进行二分查找：
+		前提该集合一定要有序。
+		int binarySearch(list,key);
+		//必须根据元素自然顺序对列表进行升级排序
+		//要求list 集合中的元素都是Comparable 的子类。
+		int binarySearch(list,key,Comparator);
+	2，对list集合进行排序。
+		sort(list); 
+		//对list进行排序,其实使用的事list容器中的对象的compareTo方法
+		sort(list,comaprator);
+		//按照指定比较器进行排序
+	3，对集合取最大值或者最小值。
+		max(Collection)
+		max(Collection,comparator)
+		min(Collection)
+		min(Collection,comparator)
+	4，对list集合进行反转。
+		revcerse(list);
+	8，可以将不同步的集合变成同步的集合。
+		Set synchronizedSet(Set<T> s)
+		Map synchronizedMap(Map<K,V> m)
+		List synchronizedList(List<T> list)
+		
+		
+		
+	5，对比较方式进行强行逆转。
+		Comparator reverseOrder();
+		Comparator reverseOrder(Comparator);
+	6，对list集合中的元素进行位置的置换。
+		swap(list,x,y);
+	7，对list集合进行元素的替换。如果被替换的元素不存在，那么原集合不变。
+		replaceAll(list,old,new);
+		
+	9. 如果想要将集合变数组：
+		可以使用Collection 中的toArray 方法。注意：是Collection不是Collections工具类
+		传入指定的类型数组即可，该数组的长度最好为集合的size。
+
+
+ * */
+public class MyDemo7 {
+	public static void main(String[] args) {
+		ArrayList<Integer> arrayList=new ArrayList<>();
+		arrayList.add(12);
+		arrayList.add(1);
+		arrayList.add(9);
+		arrayList.add(12);
+		arrayList.add(4);
+		Collections.sort(arrayList);
+		System.out.println(arrayList);
+		System.out.println("元素的索引值是："+Collections.binarySearch(arrayList, 4));
+		System.out.println("最大值："+Collections.max(arrayList));
+		System.out.println("最小值："+Collections.min(arrayList));
+		Collections.reverse(arrayList);
+		System.out.println("反转后："+arrayList);
+		arrayList=(ArrayList<Integer>) Collections.synchronizedList(arrayList);
+		
+		System.out.println("-------------------------------------------------------------");
+		
+		
+		//如果集合存储的是不具备自然顺序的元素，那么需要比较器
+		ArrayList<MyPerson> arrayList2=new ArrayList<>();
+		arrayList2.add(new MyPerson("小明", 12));
+		arrayList2.add(new MyPerson("小红", 9));
+		arrayList2.add(new MyPerson("小黑", 19));
+		arrayList2.add(new MyPerson("小白", 12));
+		Collections.sort(arrayList2,new AgeComparator());
+		System.out.println(arrayList2);
+		System.out.println("元素的索引值是："+Collections.binarySearch(arrayList2, new MyPerson("XX", 19),new AgeComparator()));
+		System.out.println("最大值："+Collections.max(arrayList2,new AgeComparator()));
+		System.out.println("最小值："+Collections.min(arrayList2,new AgeComparator()));
+		Collections.reverse(arrayList2);
+		System.out.println("反转后："+arrayList2);
+	}
+}
+
+class MyPerson{
+	String name;
+	int age;
+	public MyPerson(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "[name=" + name + ", age=" + age + "]";
+	}
+}
+class AgeComparator implements Comparator<MyPerson>{
+
+	@Override
+	public int compare(MyPerson o1, MyPerson o2) {
+		return o1.age-o2.age;
+	}
+	
+}
---
+++ src/com/cl/javabasis/day17map/MyDemo8.java
@@ -0,0 +1,51 @@
+package com.cl.javabasis.day17map;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Arrays:用于对数组操作的工具类
+	1，二分查找,数组需要有序
+		binarySearch(int[])
+		binarySearch(double[])
+	
+	2，数组排序
+		sort(int[])
+		sort(char[])……
+	2，将数组变成字符串。
+	 	toString(int[])
+	3，复制数组。
+	 	copyOf(boolean[] original,int newLength);
+	 	original 源数组
+	 	newLength 新数组长度
+	4，复制部分数组。
+		copyOfRange(boolean[] original, int from, int to)
+		original 源数组
+		from 开始拷贝的索引值
+		to 结束的索引值
+	5，比较两个数组对应位置的元素是否一致。
+		equals(int[],int[]);
+	6，将数组变成集合。
+		List asList(T[]);
+		这样可以通过集合的操作来操作数组中元素，
+		但是不可以使用增删方法，add，remove。因为数组长度是固定的，会出现
+		UnsupportOperationExcetion。
+		可以使用的方法：contains，indexOf。。。
+		如果数组中存入的基本数据类型，那么asList会将数组实体作为集合中的元素。
+		如果数组中的存入的引用数据类型，那么asList会将数组中的元素作为集合中
+		的元素。
+ */
+public class MyDemo8 {
+	public static void main(String[] args) {
+		Integer[] arr={2,4,8,5,1,15,7};
+		Integer[] arr2=/*{2,4,8,1,5,15,7};*/Arrays.copyOf(arr,10);
+		System.out.println(Arrays.toString(arr2));
+		System.out.println(Arrays.toString(Arrays.copyOfRange(arr, 1, 9)));
+		
+		System.out.println("判断两个数组的元素是否完全一致："+Arrays.equals(arr, arr2));//对应位置
+		
+		List<Integer> list=Arrays.asList(arr2);//数组变数组
+		list.toArray();//list便数组
+		System.out.println(list);
+	}
+}
