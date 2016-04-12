2cc5856ec9fd7737675f13deb4bfc2381c7107ed
Lhome <1064134436@qq.com>
2016-04-12 20:24:02 +0800

    文拷贝图片问题

--- src/com/cl/javabasis/day20/MyDemo4.java
+++ src/com/cl/javabasis/day20/MyDemo4.java
@@ -17,15 +17,18 @@ public class MyDemo4 {
 			File sourceFile=new File("E:/手机/照片/照片2236.jpg");
 			File imageFile=new File("F:/wang.jpg");
 			FileInputStream inputStream=new FileInputStream(sourceFile);
+			//每新创建一个FileOutputStream的时候，默认情况下FileOutputStream的指针是指向了文件的开始位置。
+			//每次写出一次，指针都会出现相应的移动，加true，光标指向末尾位置（追加）
 			FileOutputStream outputStream=new FileOutputStream(imageFile);
 			
 			int leng=0;
 			byte[] buf=new byte[1024];
 			while((leng=inputStream.read(buf))!=-1){
-				outputStream.write(buf);
+				outputStream.write(buf,0,leng);
 			}
-			inputStream.close();
+			//关闭资源	原则：先开后关，后开先关
 			outputStream.close();
+			inputStream.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
