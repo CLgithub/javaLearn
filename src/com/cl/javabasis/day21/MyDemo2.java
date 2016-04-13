668ffaba0c1b78f77c1e9cc48fa0abd8777af379
Lhome <1064134436@qq.com>
2016-04-13 21:45:39 +0800

    输入字符流Reader

---
+++ src/com/cl/javabasis/day21/MyDemo2.java
@@ -0,0 +1,64 @@
+package com.cl.javabasis.day21;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+/*
+ 
+ */
+public class MyDemo2 {
+	public static void main(String[] args) {
+		try {
+//			writeTest();
+			readTest();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	//使用字节流读取中文
+	public static void readTest() throws IOException{
+		//找到目标文件
+		File file=new File("E:/aaa/ccc.txt");
+		//建立数据输出通道
+		FileInputStream fileInputStream=new FileInputStream(file);
+//		int leng=0;
+//		while((leng=fileInputStream.read())!=-1){
+//			//出现乱码的原因：一个中文在UTF-8(项目设置的编码)码表中占3个字节，属于不是一个完整的中文，所以乱码
+//			System.out.print((char)leng);
+//		}
+		
+//		byte[] buf=new byte[3];
+//		for(int i=0;i<3;i++){
+//			fileInputStream.read(buf);
+//			System.out.print(new String(buf));
+//		}
+		
+		int leng=0;
+		byte[] buf=new byte[1024];
+		while((leng=fileInputStream.read(buf))!=-1){
+			System.out.print(new String(buf,0,leng));
+		}
+		
+		
+		fileInputStream.close();
+	}
+	
+	//使用字节流写中文	字节流之所以能写中文，是因为借助了字符串的getBytes方法对字符串进行了编码（字符-->数字）
+	public static void writeTest() throws IOException {
+		//找到目标文件
+		File file=new File("E:/aaa/ccc.txt");
+		//建立数据输出通道
+		FileOutputStream fileOutputStream=new FileOutputStream(file);
+		//准备数据，把数据写出
+		String data="大家好";
+		byte[] bs=data.getBytes();
+		System.out.println(Arrays.toString(bs));
+		fileOutputStream.write(bs);
+		
+		fileOutputStream.close();
+	}
+}
---
+++ src/com/cl/javabasis/day21/MyDemo3.java
@@ -0,0 +1,51 @@
+package com.cl.javabasis.day21;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+ 字节流：字节流读取的是文件中的二进制数据，读取的数据并不会帮你转换成你看得到的字符
+ 
+ 字符流：字符流会把读取到的二进制数据进行对应的编码与解码工作，字符流=字节流+编码与解码
+ 
+ 输入字符流：
+ -----| Reader 输入字符流的基类	抽象类
+ ----------| FileReader 读取文件的输入字符流
+ 
+ 
+ FileReader的用法：
+ 	1.找到目标文件
+ 	2.建立数据输出通道
+ 
+ */
+public class MyDemo3 {
+	public static void main(String[] args) {
+		try {
+			readTest1();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void readTest1() throws IOException {
+		//找到目标文件
+//		File file=new File("E:/javaProject/javaLearn/src/com/cl/javabasis/day8/MyDemo1.java");
+		File file=new File("E:/学习视频/2015传智基础33/day21/说明.txt");
+		//建立数据输出通道
+		FileReader fileReader=new FileReader(file);
+//		int centent=fileReader.read();
+//		System.out.print((char)centent);
+		
+		//使用缓存字符数组读取文件
+		int leng=0;
+		char[] buf=new char[1024];
+		while((leng=fileReader.read(buf))!=-1){
+			System.out.println(new String(buf,0,leng));
+		}
+		
+		fileReader.close();
+	}
+}
